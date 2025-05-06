678cd96b2522b0d15b0f46fdc7d8ffa5874577bf
ChityaVerepanov <cherepanov0077@gmail.com>
2025-05-06 15:32:37 +0500

    Check rebuild

--- buildme.sh
+++ buildme.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git pull
+
 export CACHE_BUST=$(date +"%Y%H%M%S")
 #echo "Docker containers restarting with rebuild ${CACHE_BUST}"
 docker compose build --build-arg CACHE_BUST=${CACHE_BUST}
