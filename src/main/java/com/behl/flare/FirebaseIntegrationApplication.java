2c7d04fca4bccda3634c252f72ded8ba9cb429cd
ChityaVerepanov <cherepanov0077@gmail.com>
2025-05-06 13:43:50 +0500

    Initial commit

---
+++ src/main/java/com/behl/flare/FirebaseIntegrationApplication.java
@@ -0,0 +1,17 @@
+package com.behl.flare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Swagger:
+ * http://localhost:8855/swagger-ui/index.html
+ */
+@SpringBootApplication
+public class FirebaseIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebaseIntegrationApplication.class, args);
+	}
+
+}
