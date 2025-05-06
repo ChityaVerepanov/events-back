3cf7ddf3e0d06667c3cd2bf53d596a3d03aed416
ChityaVerepanov <cherepanov0077@gmail.com>
2025-05-06 16:07:43 +0500

    Check rebuild

--- buildme.sh
+++ buildme.sh
@@ -3,8 +3,10 @@
 git fetch origin
 git reset --hard origin/main
 #git pull
+echo "Repository loaded..."
 
 chmod +x -- "$0"
+echo "Script permission restored. If failed? start manually: 'chmod +x buildme.sh'"
 
 export CACHE_BUST=$(date +"%Y%H%M%S")
 #echo "Docker containers restarting with rebuild ${CACHE_BUST}"
